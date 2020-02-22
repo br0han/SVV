0da0feafda3170880e4ced746c3cb5e4cb213225
alessandrosimonelli10 <alessandro.simonelli10@gmail.com>
2020-02-22 11:38:00 +0100

    changes

--- Simulation/DistributedForceTorqueXaxis.py
+++ Simulation/DistributedForceTorqueXaxis.py
@@ -21,10 +21,6 @@ Coord = np.array(Coord)
 """
 MatrixF , MatrixT , MatrixMF, MatrixMT = DistForceTorqueMatrix() #data,Coord,ShearCenter
 
-<<<<<<< HEAD
-=======
-MatrixF , MatrixT , MatrixMF, MatrixMT = DistForceTorqueMatrix()
->>>>>>> 9c13501ddf095838e1e6df9f27ff586170a31694
 
 n = len(MatrixF)+1
 Ca = GC.Ca #0.484
@@ -79,11 +75,9 @@ plt.subplot(1,2,1)
 plt.plot(XX,FF)
 plt.legend('Force')
 
-'''plt.subplot(1,2,2)
+plt.subplot(1,2,2)
 plt.plot(XX,TT)
 plt.legend('Torque')
-'''
-plt.grid(1)
 
 plt.show()
 
